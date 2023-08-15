e872449c283b66d20c8f06e55048a1f6695de3af
Gustave TSOPMO <gustaveckt@gmail.com>
2023-08-15 21:09:19 +0100

    Updating auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import { dark } from "@clerk/themes";
+
 import "../globals.css";
 
 export const metadata = {
@@ -15,10 +17,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
         <body className={`${inter.className} bg-dark-1`}>
-          <div className="w-full flex justify-center items-center">
+          <div className="w-full flex justify-center items-center min-h-screen">
             {children}
           </div>
         </body>
